b3889018c657535314474c12cb9f7e2765a08500
osierra123 <93794335+osierra123@users.noreply.github.com>
2021-11-05 15:27:10 -0500

    Add files via upload

---
+++ Pot assingment/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * //TODO: Add your name
+ * Recursion Assignment
+ * //TODO: Add date
+ * Based on Dogon Pot pseudo code from the book
+ * African Fractals: Modern Computing and Indigenous Design by Ron Eglash
+ */
+
+using System;
+
+namespace DogonPotVariation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Example steps to draw a Dogon-like pot.";
+            new Pot();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Pot assingment/pot.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DogonPotVariation
+{
+    class Pot
+    {
+        //private means that only members of this class
+        //can access (read or change) these variables
+        private int diameter = 9;
+        private int minimumDiameter = 1;
+        private string measurementSystem = "inches";
+        private int step = 1;
+
+        //Constructor
+        //When an instance is made, the statements in this method
+        //will run first, and only once
+        public Pot()
+        {
+            Console.WriteLine("Steps to draw a Dogon-like pot:");
+            DrawPot();
+        }
+
+        private void DrawPot()
+        {
+            //if-then conditional statement
+            if (diameter <= minimumDiameter)
+            {
+                Console.WriteLine($"{step}. Draw the lid");
+            }
+            else
+            {
+                Console.WriteLine($"{step}. Draw a circle that is {diameter} {measurementSystem} in size");
+
+                //TODO: Add code to reduce the value of the variable diameter by 2/3
+                diameter = diameter / 3;
+
+
+                //TODO: increment value of the variable step
+                step++;
+
+                //TODO: add a recursive function/method call to invoke this method again
+                DrawPot();
+            }
+        }
+
+    }
+}
